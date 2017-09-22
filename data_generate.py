61f0a6c45dbaa3ac80cca431d9d9d07d2a39f94d
michaelscottfield <jr1111@mail.ustc.edu.cn>
2017-09-22 11:12:29 +0800

    finished

--- data_generate.py
+++ data_generate.py
@@ -1,5 +1,7 @@
 # coding=utf-8
-
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
 fr = open('../work/work919/heartdisease.txt', 'r')
 frr = open('../work/work919/dict.txt', 'r')
 fw = open('../work/work919/train.data', 'w')
@@ -11,6 +13,7 @@ tm = []
 cha = []
 din = []
 for line in frr.readlines():
+    line = line.decode("utf-8")
     x = line.strip('\n').strip(' ')
     x1 = x.split(' ')[0]
     x2 = x.split(' ')[1]
@@ -29,127 +32,150 @@ for line in frr.readlines():
     if x2 == 'din':
         din.append(x1)
 
-for linee in fr.readlines():
-    x = linee.strip('\n')
-    #y = unicode(x, "gb2312")
-
-    #print linee.decode("gb2312")
-    for i in range(0, len(x)):
-        fw.write(x[i])
-    ''' for term in sym:
+for line in fr.readlines():
+    line = line.decode("utf-8")
+    x = line.strip('\n')
+    
+    for xchar in x:
+        flag = 0
+        for term in sym:
             if xchar in term:
                 if xchar == term:
-                    #fw.write(xchar + 'sym-S')
+                    fw.write(xchar + ' sym-S\n')
+                    flag = 1
                     break
                 if xchar == term[0]:
-                    fw.write(xchar)
-                    #fw.write(term[0] + 'ok')
+                    fw.write(xchar + ' sym-B\n')
+                    flag = 1
                     break
                 if xchar == term[-1]:
-                    #fw.write(xchar + 'sym-E')
+                    fw.write(xchar + ' sym-E\n')
+                    flag = 1
                     break
                 else:
-                    #fw.write(xchar + 'sym-M')
-                    break'''
-    '''for term in dis:
+                    fw.write(xchar + ' sym-M\n')
+                    flag = 1
+                    break
+        if flag == 1:
+            break
+        for term in dis:
             if xchar in term:
                 if xchar == term:
-                    fw.write(xchar + 'dis-S')
+                    fw.write(xchar + ' dis-S\n')
+                    flag = 1
                     break
                 if xchar == term[0]:
-                    fw.write(xchar + 'dis-B')
+                    fw.write(xchar + ' dis-B\n')
+                    flag = 1
                     break
                 if xchar == term[-1]:
-                    fw.write(xchar + 'dis-E')
+                    fw.write(xchar + ' dis-E\n')
+                    flag = 1
                     break
                 else:
-                    fw.write(xchar + 'dis-M')
+                    fw.write(xchar + ' dis-M\n')
+                    flag = 1
                     break
-            else:
-                fw.write(xchar + 'O')
-                break
+        if flag == 1:
+            break
         for term in cty:
             if xchar in term:
                 if xchar == term:
-                    fw.write(xchar + 'cty-S')
+                    fw.write(xchar + ' cty-S\n')
+                    flag = 1
                     break
                 if xchar == term[0]:
-                    fw.write(xchar + 'cty-B')
+                    fw.write(xchar + ' cty-B\n')
+                    flag = 1
                     break
                 if xchar == term[-1]:
-                    fw.write(xchar + 'cty-E')
+                    fw.write(xchar + ' cty-E\n')
+                    flag = 1
                     break
                 else:
-                    fw.write(xchar + 'cty-M')
+                    fw.write(xchar + ' cty-M\n')
+                    flag = 1
                     break
-            else:
-                fw.write(xchar + 'O')
-                break
+        if flag == 1:
+            break
         for term in des:
             if xchar in term:
                 if xchar == term:
-                    fw.write(xchar + 'des-S')
+                    fw.write(xchar + ' des-S\n')
+                    flag = 1
                     break
                 if xchar == term[0]:
-                    fw.write(xchar + 'des-B')
+                    fw.write(xchar + ' des-B\n')
+                    flag = 1
                     break
                 if xchar == term[-1]:
-                    fw.write(xchar + 'des-E')
+                    fw.write(xchar + ' des-E\n')
+                    flag = 1
                     break
                 else:
-                    fw.write(xchar + 'des-M')
+                    fw.write(xchar + ' des-M\n')
+                    flag = 1
                     break
-            else:
-                fw.write(xchar + 'O')
-                break    
+        if flag == 1:
+            break
         for term in tm:
             if xchar in term:
                 if xchar == term:
-                    fw.write(xchar + 'tm-S')
+                    fw.write(xchar + ' tm-S\n')
+                    flag = 1
                     break
                 if xchar == term[0]:
-                    fw.write(xchar + 'tm-B')
+                    fw.write(xchar + ' tm-B\n')
+                    flag = 1
                     break
                 if xchar == term[-1]:
-                    fw.write(xchar + 'tm-E')
+                    fw.write(xchar + ' tm-E\n')
+                    flag = 1
                     break
                 else:
-                    fw.write(xchar + 'tm-M')
+                    fw.write(xchar + ' tm-M\n')
+                    flag = 1
                     break
-            else:
-                fw.write(xchar + 'O')
-                break 
+        if flag == 1:
+            break
         for term in cha:
             if xchar in term:
                 if xchar == term:
-                    fw.write(xchar + 'cha-S')
+                    fw.write(xchar + ' cha-S\n')
+                    flag == 1
                     break
                 if xchar == term[0]:
-                    fw.write(xchar + 'cha-B')
+                    fw.write(xchar + ' cha-B\n')
+                    flag == 1
                     break
                 if xchar == term[-1]:
-                    fw.write(xchar + 'cha-E')
+                    fw.write(xchar + ' cha-E\n')
+                    flag == 1
                     break
                 else:
-                    fw.write(xchar + 'cha-M')
+                    fw.write(xchar + ' cha-M\n')
+                    flag == 1
                     break
-            else:
-                fw.write(xchar + 'O')
-                break 
+        if flag == 1:
+            break
         for term in din:
             if xchar in term:
                 if xchar == term:
-                    fw.write(xchar + 'din-S')
+                    fw.write(xchar + ' din-S\n')
+                    flag = 1
                     break
                 if xchar == term[0]:
-                    fw.write(xchar + 'din-B')
+                    fw.write(xchar + ' din-B\n')
+                    flag = 1
                     break
                 if xchar == term[-1]:
-                    fw.write(xchar + 'din-E')
+                    fw.write(xchar + ' din-E\n')
+                    flag = 1
                     break
                 else:
-                    fw.write(xchar + 'din-M')
+                    fw.write(xchar + ' din-M\n')
+                    flag = 1
                     break
-            else:
-                fw.write(xchar + 'O')
-                break '''
+        if flag == 1:
+            break
+        fw.write(xchar + ' O\n')
